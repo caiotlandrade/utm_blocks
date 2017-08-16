5a04e8d30456bc568093e609ab2ec8ced2868dae
Caio Andrade <CaioAndrade@MacBook-Pro-de-Caio.local>
2017-08-16 15:14:38 -0400

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App_Header_Nav from './Header_Nav';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<App_Header_Nav />, document.getElementById('root'));
+registerServiceWorker();

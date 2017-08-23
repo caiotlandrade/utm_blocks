4875502e97311bc2090f4fc82e564b1c945713ce
Caio Andrade <CaioAndrade@MacBook-Pro-de-Caio.local>
2017-08-23 12:18:05 -0400

    deleted initial state and temporary end point

--- server/server.js
+++ server/server.js
@@ -29,63 +29,6 @@ app.use('/api/url', url_routes);
 
 
 
-// //initial state that is sent to the client after request
-// // temporary solution before building and plugin the DB 
-// var initialState = {
-//     urls: [
-//         {
-//             key: 0,
-//             created_at: 'now',
-//             updated_at: 'now',
-//             original_url: 'https://www.example.com',
-//             campaign_source: 'google',
-//             campaign_medium: 'cpm',
-//             campaign_name: 'example',
-//             campaign_term: 'ex',
-//             campaign_content: 'ex2',
-//             final_url: 'https://www.example.com/?utm_source=google&utm_medium=cpc',
-//             shortened_url: 'https://goo.gl/ebqs2I',
-//             account_key: 0,
-//             user_key: 0
-//         }
-//     ],
-//     websites: [
-//         {
-//             key: 0,
-//             created_at: 'now',
-//             updated_at: 'now',
-//             website: 'https://www.example.com',
-//             account_key: 0,
-//             user_key: 0
-//         }
-//     ],
-//     sources: [
-//         {
-//             key: 0,
-//             created_at: 'now',
-//             updated_at: 'now',
-//             source: 'google',
-//             account_key: 0,
-//             user_key: 0
-//         }
-//     ],
-//     media: [
-//         {
-//             key: 0,
-//             created_at: 'now',
-//             updated_at: 'now',
-//             medium: 'cpc',
-//             account_key: 0,
-//             user_key: 0
-//         }
-//     ]
-// };
-
-// // sending initial state to the client
-// app.get('/api/initialState', (req, res) => {
-//     res.json(initialState);
-//     console.log(initialState);
-// });
 
 // Defining port using setup for Heroku
 app.listen(PORT, () => {
